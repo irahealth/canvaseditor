e0a6bd3ae1c201b4ba1270a9a02354665030afd4
irahealth <>
2021-05-14 12:06:15 +0000

    Initial commit

---
+++ line.js
@@ -0,0 +1,8 @@
+
+
+export function drawLine(ctx) {
+ctx.moveTo(0, 0);
+ctx.lineTo(200, 100);
+ctx.stroke();
+}
+
---
+++ script.js
@@ -0,0 +1,12 @@
+import * as line  from './line.js';
+
+
+var c = document.getElementById("myCanvas");
+var ctx = c.getContext("2d");
+
+
+
+
+line.drawLine(ctx);
+
+
